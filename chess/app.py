378eda1f36b4988d1f6ec18f6a68a25435c4af46
Evan Kim <evan.e.kim@gmail.com>
2025-06-12 19:21:45 -0700

    some puzzle and command center logic

--- chess/app.py
+++ chess/app.py
@@ -1,8 +1,64 @@
-from flask import Flask, jsonify, render_template
+import hashlib
+import os
+import base64
+
+from flask import Flask, jsonify, render_template, request
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
+
+#### command center stuff ####
+
+PUZZLE_SECRET = os.getenv("PUZZLE_SECRET") # TODO: fix these based on true config
+SECRET_KEY = os.getenv("SECRET_KEY") # TODO: fix these based on true config
+
+def get_flag(user_id):
+    return hashlib.sha256(
+        f"{user_id}_{PUZZLE_SECRET}".encode("utf-8")
+    ).hexdigest()
+
+
+def verify_username(uname):
+    if not uname:
+        return False
+    if '_' not in uname:
+        return False
+    parts = uname.split("_")
+    if len(parts) != 2:
+        return False
+    return hashlib.sha256(
+        f"{parts[0]}_{SECRET_KEY}".encode("utf-8")
+    ).hexdigest()[:8] == parts[1]
+    
+    
+#### puzzle stuff ####
+
+FULL_GAME_PGN = "asdf"
+INSTRUCTION = "walk the king counterclockwise"
+
+def get_caption(user_id):
+    flag = get_flag(user_id)
+    padding_length = len(FULL_GAME_PGN) - (len(INSTRUCTION) + len("_Submit this token: ") + len(flag))
+    full_plaintext = f"{INSTRUCTION}" + "_" * padding_length + f"Submit this token: {flag}"
+    
+    # Base64 encode FULL_GAME_PGN
+    encoded_pgn = base64.b64encode(FULL_GAME_PGN.encode('utf-8')).decode('utf-8')
+    
+    # Base64 encode full_plaintext
+    encoded_plaintext = base64.b64encode(full_plaintext.encode('utf-8')).decode('utf-8')
+    
+    # XOR the two base64 encoded strings
+    ciphertext = ""
+    for i in range(len(encoded_pgn)):
+        if i < len(encoded_plaintext):
+            ciphertext += chr(ord(encoded_pgn[i]) ^ ord(encoded_plaintext[i]))
+        else:
+            ciphertext += encoded_pgn[i]
+    
+    return full_plaintext
+
+#### api ####
 
 @app.route('/')
 def index():
@@ -10,8 +66,9 @@ def index():
 
 @app.route('/get_caption', methods=['GET'])
 def get_caption():
-    # Placeholder caption - can be modified later
-    caption = "VGhpcyBpcyBhIHBsYWNlaG9sZGVyIGNhcHRpb24="
+    data = request.json
+    user_id = data.get("userId", "")
+    caption = get_caption(user_id)
     return jsonify({'caption': caption})
 
 if __name__ == '__main__':
