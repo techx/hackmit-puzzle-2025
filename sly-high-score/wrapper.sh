b24451a869c1edfffceda63d436780421d90aacf
Spencer Pogorzelski <34356756+spencerpogo@users.noreply.github.com>
2025-06-26 14:40:52 -0400

    impl user auth for shs

---
+++ sly-high-score/wrapper.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+: "${PUZZLE_SECRET:?PUZZLE_SECRET environment variable not set -- contact admins if this is the official deployment}"
+
+printf "user ID: "
+read -r USER_ID
+flag_inner="$(printf '%s_%s' "$USER_ID" "$PUZZLE_SECRET" | sha256sum - | awk '{print $1}')"
+printf 'hack{%s}\n' "$flag_inner" > /tmp/flag
+
+exec /app/challenge.sh
