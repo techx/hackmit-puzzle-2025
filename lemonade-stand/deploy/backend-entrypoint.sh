aa0fc06bba3468dbac0e9ffa2d90e2688b90f448
Spencer Pogorzelski <34356756+spencerpogo@users.noreply.github.com>
2025-06-26 18:14:35 -0400

    impl handshake wrapper

---
+++ lemonade-stand/deploy/backend-entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+: "${PUZZLE_SECRET:?PUZZLE_SECRET environment variable not set -- contact admins if this is the official deployment}"
+
+cd /tmp
+printf "user ID: "
+read -r USER_ID
+flag_inner="$(printf '%s_%s' "$USER_ID" "$PUZZLE_SECRET" | sha256sum - | awk '{print $1}')"
+printf 'hack{%s}\n' "$flag_inner" > ./flag
+
+exec /app/lemonade_stand
